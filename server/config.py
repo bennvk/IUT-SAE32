0bbb96ac5f1fefbbeb03fab96e854aa35fbed5b1
benjamin <210059635+bennvk@users.noreply.github.com>
2026-03-02 08:54:02 +0100

    Define BASE_PATH and PROJECT_ROOT constants

--- server/config.py
+++ server/config.py
@@ -2,6 +2,8 @@
 
 import os
 
+BASE_PATH = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(BASE_PATH)
 PATH_DOCUMENTS = os.path.join(PROJECT_ROOT, "documents")
 SERVER_HOST = "127.0.0.1"
 SERVER_PORT = 5000
